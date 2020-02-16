1596793b7cf13afc26dae84fec9b629b5bb19bc0
ilya <ilyamaslou@gmail.com>
2020-02-17 01:23:48 +0300

    init

---
+++ MovieWorld/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  MovieWorld
+//
+//  Created by Ilya Maslou on 2/17/20.
+//  Copyright © 2020 Ilya Maslou. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
---
+++ MovieWorld/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MovieWorld
+//
+//  Created by Ilya Maslou on 2/17/20.
+//  Copyright © 2020 Ilya Maslou. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+

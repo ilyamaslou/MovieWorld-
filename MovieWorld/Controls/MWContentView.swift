852969cfbd2cfa113fb267c0278db501715415a8
ilya <ilyamaslou@gmail.com>
2020-02-17 16:01:36 +0300

    did realized borring way of creating constraints

--- MovieWorld/Controls/MWContentView.swift
+++ MovieWorld/Controls/MWContentView.swift
@@ -17,11 +17,11 @@ class MWContentView: UIView {
     private let film = MWFilm()
     
     private let containerView: UIView = {
-           let view = UIView()
-           view.backgroundColor = .white
-
-           return view
-       }()
+        let view = UIView()
+        view.backgroundColor = .white
+        
+        return view
+    }()
     
     private lazy var filmImageView: UIImageView = {
         let view = UIImageView()
@@ -49,6 +49,7 @@ class MWContentView: UIView {
     private lazy var genresLable: UILabel = {
         let label = UILabel()
         label.font = .systemFont(ofSize: 15, weight: .light)
+        label.textColor = .gray
         
         var genres = ""
         for genre in film.filmGenres {
@@ -62,14 +63,12 @@ class MWContentView: UIView {
     }()
     
     private lazy var ratingsLable: UILabel = {
-           let label = UILabel()
-           label.font = .systemFont(ofSize: 15)
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 15)
         var ratings = "IMBD \(film.imbdRating), KP \(film.kpRating)"
         label.text = ratings
-           return label
-       }()
-    
-    // MARK: - init
+        return label
+    }()
     
     init() {
         super.init(frame: .zero)
@@ -79,47 +78,96 @@ class MWContentView: UIView {
         containerView.addSubview(releaseYearAndCountryLable)
         containerView.addSubview(genresLable)
         containerView.addSubview(ratingsLable)
+        
+        self.makeConstraints()
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
     
-    override func updateConstraints() {
+    //    override func updateConstraints() {
+    //
+    //        self.filmImageView.snp.updateConstraints { (make) in
+    //            make.top.left.equalToSuperview().inset(insets)
+    //            make.size.equalTo(imageSize)
+    //        }
+    //
+    //        self.containerView.snp.updateConstraints { (make) in
+    //            make.left.equalTo(filmImageView.snp.right).inset(-16)
+    //            make.top.equalToSuperview().inset(insets)
+    //            make.bottom.equalTo(filmImageView.snp.bottom)
+    //
+    //        }
+    //
+    //        self.filmNameLabel.snp.updateConstraints { (make) in
+    //            make.top.left.equalToSuperview().inset(contentInsets)
+    //        }
+    //
+    //        self.releaseYearAndCountryLable.snp.updateConstraints { (make) in
+    //            make.top.equalTo(filmNameLabel.snp.bottom).offset(8)
+    //            make.left.equalTo(filmNameLabel.snp.left)
+    //
+    //        }
+    //
+    //        self.genresLable.snp.updateConstraints { (make) in
+    //            make.top.equalTo(releaseYearAndCountryLable.snp.bottom).offset(6)
+    //            make.left.equalTo(releaseYearAndCountryLable.snp.left)
+    //        }
+    //
+    //        self.ratingsLable.snp.updateConstraints { (make) in
+    //            make.left.equalTo(genresLable.snp.left)
+    //            make.bottom.equalToSuperview()
+    //        }
+    //
+    //        super.updateConstraints()
+    //    }
+    
+    private func makeConstraints() {
+        self.containerView.translatesAutoresizingMaskIntoConstraints = false
+        self.filmImageView.translatesAutoresizingMaskIntoConstraints = false
+        self.filmNameLabel.translatesAutoresizingMaskIntoConstraints = false
+        self.releaseYearAndCountryLable.translatesAutoresizingMaskIntoConstraints = false
+        self.genresLable.translatesAutoresizingMaskIntoConstraints = false
+        self.ratingsLable.translatesAutoresizingMaskIntoConstraints = false
         
-        filmImageView.snp.updateConstraints { (make) in
-            make.top.left.equalToSuperview().inset(insets)
-            make.size.equalTo(imageSize)
-        }
+        let topImageViewConstraint = NSLayoutConstraint(item: filmImageView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: insets.top)
+        let leftImageViewConstraint = NSLayoutConstraint(item: filmImageView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: insets.left)
+        let imageWidth = NSLayoutConstraint(item: filmImageView, attribute: NSLayoutConstraint.Attribute.width, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: NSLayoutConstraint.Attribute.notAnAttribute, multiplier: 1, constant: imageSize.width)
+        let imageHeight = NSLayoutConstraint(item: filmImageView, attribute: NSLayoutConstraint.Attribute.height, relatedBy: NSLayoutConstraint.Relation.equal, toItem: nil, attribute: .notAnAttribute, multiplier: 1, constant: imageSize.height)
         
-        containerView.snp.updateConstraints { (make) in
-            make.left.equalTo(filmImageView.snp.right).inset(-16)
-            make.top.equalToSuperview().inset(insets)
-            make.bottom.equalTo(filmImageView.snp.bottom)
-            
-        }
+        let topContainerConstraint = NSLayoutConstraint(item: containerView, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: self, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: insets.top)
+        let leftContainerConstraint = NSLayoutConstraint(item: containerView, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: filmImageView, attribute: NSLayoutConstraint.Attribute.right, multiplier: 1, constant: insets.left)
+        let bottomContainerConstraint = NSLayoutConstraint(item: containerView, attribute: NSLayoutConstraint.Attribute.bottom, relatedBy: NSLayoutConstraint.Relation.equal, toItem: filmImageView, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 0)
         
-        filmNameLabel.snp.updateConstraints { (make) in
-            make.top.left.equalToSuperview().inset(contentInsets)
-        }
-
-        releaseYearAndCountryLable.snp.updateConstraints { (make) in
-            make.top.equalTo(filmNameLabel.snp.bottom).offset(8)
-            make.left.equalTo(filmNameLabel.snp.left)
-
-        }
-
-        genresLable.snp.updateConstraints { (make) in
-            make.top.equalTo(releaseYearAndCountryLable.snp.bottom).offset(6)
-            make.left.equalTo(releaseYearAndCountryLable.snp.left)
-        }
+        let topFilmNameLabelConstraint = NSLayoutConstraint(item: filmNameLabel, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: containerView, attribute: NSLayoutConstraint.Attribute.top, multiplier: 1, constant: contentInsets.top)
+        let leftFilmNameLabelConstraint = NSLayoutConstraint(item: filmNameLabel, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: containerView, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
         
-        ratingsLable.snp.updateConstraints { (make) in
-            make.left.equalTo(genresLable.snp.left)
-            make.bottom.equalToSuperview()
-        }
-
-        super.updateConstraints()
+        let topReleaseYearAndCountryLabelConstraint = NSLayoutConstraint(item: releaseYearAndCountryLable, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: filmNameLabel, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 8)
+        let leftReleaseYearAndCountryLabelConstraintConstraint = NSLayoutConstraint(item: releaseYearAndCountryLable, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: filmNameLabel, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
+        
+        let topGenresLabelConstraint = NSLayoutConstraint(item: genresLable, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: releaseYearAndCountryLable, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: 6)
+        let leftGenresLabelConstraint = NSLayoutConstraint(item: genresLable, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: releaseYearAndCountryLable, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
+        
+        let bottomRatingLabelConstraint = NSLayoutConstraint(item: ratingsLable, attribute: NSLayoutConstraint.Attribute.top, relatedBy: NSLayoutConstraint.Relation.equal, toItem: containerView, attribute: NSLayoutConstraint.Attribute.bottom, multiplier: 1, constant: -16)
+        let leftRatingLabelConstraint = NSLayoutConstraint(item: ratingsLable, attribute: NSLayoutConstraint.Attribute.left, relatedBy: NSLayoutConstraint.Relation.equal, toItem: genresLable, attribute: NSLayoutConstraint.Attribute.left, multiplier: 1, constant: 0)
+        
+        self.containerView.addConstraints([topFilmNameLabelConstraint,
+                                           leftFilmNameLabelConstraint,
+                                           topReleaseYearAndCountryLabelConstraint,
+                                           leftReleaseYearAndCountryLabelConstraintConstraint,
+                                           topGenresLabelConstraint,
+                                           leftGenresLabelConstraint,
+                                           bottomRatingLabelConstraint,
+                                           leftRatingLabelConstraint])
+        
+        self.addConstraints([topImageViewConstraint,
+                             leftImageViewConstraint,
+                             topContainerConstraint,
+                             leftContainerConstraint,
+                             bottomContainerConstraint,
+                             imageWidth,
+                             imageHeight])
     }
     
 }
